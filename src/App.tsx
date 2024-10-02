dfed57f13de9aeb52d0e098d5611b6653e94e535
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-10-02 19:29:08 +0900

    update: トップページから About ページに遷移できるように改修

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
+import { Link } from '@tanstack/react-router';
 
 function App() {
 
   return (
-    <div className='h-full w-screen'>
-      <div className="flex justify-center items-center">
+    <div className='min-h-screen w-screen flex justify-center items-center'>
+      <div className="flex flex-col justify-center items-center">
         <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <Link to='/about' className='bg-blue-500 text-white p-4 mt-4'>To About</Link>
       </div>
     </div>
   );
